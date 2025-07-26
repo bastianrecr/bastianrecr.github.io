a0b5893b2488591fab5b844dd069fb2d373c08e5
Sebastian Regules <serecruz97@hotmail.com>
2025-07-25 23:18:50 -0600

    Add main block and include the projects block, the project-source block, and the project-card block to organize the projects by their source and show them as cards. Create styling for each new block, and create index.js to add functionality to each project-source header as an accordion

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+document.querySelectorAll(".project-source__header").forEach((header) => {
+  header.addEventListener("click", () => {
+    // Finds the parent container project-source
+    const sourceContainer = header.closest(".project-source");
+    if (!sourceContainer) return;
+
+    // Finds the project container inside that block
+    const projectsContainer = sourceContainer.querySelector(
+      ".project-source__content"
+    );
+    if (!projectsContainer) return;
+
+    // Alternates the state
+    const isExpanded = header.getAttribute("aria-expanded") === "true";
+    header.setAttribute("aria-expanded", String(!isExpanded));
+    projectsContainer.classList.toggle("project-source__content_show");
+    projectsContainer.setAttribute("aria-hidden", String(isExpanded));
+  });
+});
